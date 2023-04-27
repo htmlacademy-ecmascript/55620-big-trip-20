8464cc608dc3521f581210ec739ff8ec14b54c19
bugoaneo <polina_emelyanov@mail.ru>
2023-04-27 16:43:51 +0700

    dev: homeWork 1.18

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import {RenderPosition, render } from './render';
+import TripInfo from './view/trip-info';
+import FilterMenu from './view/filteres';
+import SortMenu from './view/sorting';
+import AppPresenter from './presenter/app-prezenter';
+
+const tripInfoContainer = document.querySelector('.trip-main');
+const tripFiltersContainer = document.querySelector('.trip-controls__filters');
+const tripEventsContainer = document.querySelector('.trip-events');
+const appPresenter = new AppPresenter({ appWrapper: tripEventsContainer });
+
+
+render(new TripInfo(), tripInfoContainer, RenderPosition.AFTERBEGIN);
+render(new FilterMenu(), tripFiltersContainer);
+render(new SortMenu(), tripEventsContainer);
+
+
+appPresenter.init();
