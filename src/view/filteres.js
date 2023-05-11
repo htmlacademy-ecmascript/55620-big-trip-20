0bdecb9e632cc5cdbc67991068bf6af117f10aef
bugoaneo <polina_emelyanov@mail.ru>
2023-05-11 17:12:49 +0700

    dev: 4.17. Шаблонизируй то

--- src/view/filteres.js
+++ src/view/filteres.js
@@ -1,26 +1,35 @@
 import AbstractView from '../framework/view/abstract-view';
+import {capitalize } from '../utils/commonUtils';
 
-function createFilterMenu() {
-  return `<form class="trip-filters" action = "#" method = "get" >
-                <div class="trip-filters__filter">
-                  <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-                    <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-                  </div><div class="trip-filters__filter">
-                      <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-                        <label class="trip-filters__filter-label" for="filter-future">Future</label>
-                      </></div><div class="trip-filters__filter">
-                      <input id="filter-present" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="present">
-                        <label class="trip-filters__filter-label" for="filter-present">Present</label>
-                      </></div><div class="trip-filters__filter">
-                      <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-                        <label class="trip-filters__filter-label" for="filter-past">Past</label>
-                      </></div><button class="visually-hidden" type="submit">Accept filter</button></>
-              </form>`;
+
+const createFilterItem = (filterData, isChecked) => {
+  const { type, count } = filterData;
+  return `<div class="trip-filters__filter">
+    <input id="filter-${type}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${type}" ${isChecked ? 'checked' : ''} ${count || 'disabled'}>
+    <label class="trip-filters__filter-label" for="filter-${type}">${capitalize(type)}</label>
+  </div>`;
+};
+
+function createFilterMenu(filterData) {
+
+  const createFilterList = filterData.map((item, index) => createFilterItem(item, index === 0)).join('');
+
+  return `<form class="trip-filters" action="#" method="get">
+${createFilterList}
+  <button class="visually-hidden" type="submit">Accept filter</button>
+</form>`;
 }
 
 export default class FilterMenu extends AbstractView {
+  #filterData = null;
+
+  constructor({filterData}) {
+    super();
+    this.#filterData = filterData;
+  }
+
   get template() {
-    return createFilterMenu();
+    return createFilterMenu(this.#filterData);
   }
 
 }
